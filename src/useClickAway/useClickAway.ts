393df6ba8723a6827a23b0ba8c585e0df7a00594
XeryYue <812137533@qq.com>
2021-06-07 21:25:33 +0800

    feature:(hooks) add useClickAway & useTitle

---
+++ src/useClickAway/useClickAway.ts
@@ -0,0 +1,27 @@
+import { Ref, unref } from 'vue'
+import { useEventListener } from '../useEventListener'
+
+export interface useClickAwayOptions {
+  event?: string
+}
+
+const defaultOptions: useClickAwayOptions = {
+  event: 'click',
+}
+
+const useClickAway = (
+  listener: EventListener,
+  target: Element | Ref<Element | undefined>,
+  options = defaultOptions
+) => {
+  console.log(options)
+  const onClick = (evt: Event) => {
+    const element = unref(target)
+    if (element && !element.contains(evt.target as Node)) {
+      listener(evt) 
+    }
+  }
+  useEventListener(options.event!, onClick, { target: document })
+}
+
+export { useClickAway }
